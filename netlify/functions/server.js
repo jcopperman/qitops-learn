4f762a1c73c9136de5e830def3ee78e3e1d1e53b
Jonathan Opperman <opperman.jonathan@gmail.com>
2025-04-10 13:52:05 +0200

    Fix Netlify deployment configuration with correct Remix versions

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,5 +1,8 @@
 import { createRequestHandler } from "@remix-run/netlify";
 import * as build from "../../build/server/index.js";
+import { installGlobals } from "@remix-run/node";
+
+installGlobals();
 
 export const handler = createRequestHandler({
   build,
